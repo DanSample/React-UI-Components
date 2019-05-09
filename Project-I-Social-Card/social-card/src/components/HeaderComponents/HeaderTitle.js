3b01bb1364d797ff7ea9d988110c65d23c8fd6a7
Dan Sample <danssample@gmail.com>
2019-05-08 22:32:21 -0700

    added react img

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,5 +2,12 @@ import React from 'react';
 import './Header.css';
 
 const HeaderTitle = () => {
-  return <h1>Lambda</h1>;
+  return (
+    <div id="headerTitle">
+      <strong>Lambda School</strong>
+      <p>@LambdaSchool &#8226; 26 jan</p>
+    </div>
+  );
 };
+
+export default HeaderTitle;
